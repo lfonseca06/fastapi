f6229fad0ce7ee5f23c33666e60c5d642ee624c3
lfonseca06 <167661509+lfonseca06@users.noreply.github.com>
2026-04-14 15:18:23 +0000

    cambios en bases de datos

--- app/database.py
+++ app/database.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine # type: ignore
-from sqlalchemy.ext.delcarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL = "postgresql://:postgres:admin@localhost:5432/ip_app"
 
 engine = create_engine(DATABASE_URL)
 SessionmLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
