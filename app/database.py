ad03e13b35df906db510dda50a07206086282fdd
lfonseca06 <167661509+lfonseca06@users.noreply.github.com>
2026-04-14 15:48:40 +0000

    cambios en bases de datos para conexión con Azure

--- app/database.py
+++ app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql://:postgres:admin@localhost:5432/ip_app"
+#DATABASE_URL = "postgresql://:postgres:admin@localhost:5432/ip_app"
+DATABASE_URL = "postgresql://:elxbuvzqcg:L$oDNjJY05FLlLK2@ip-app-server.postgres.database.azure.com"
+#DATABASE_URL = "cnx = psycopg2.connect(user="elxbuvzqcg", password="{your_password}", host="ip-app-server.postgres.database.azure.com", port=5432, database="ip-app")"
 
 engine = create_engine(DATABASE_URL)
 SessionmLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
